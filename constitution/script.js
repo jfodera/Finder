27cfba5721072c1a951c8fe0d4743c72f6d35878
Kagiri2 <37049496+Kagiri2@users.noreply.github.com>
2024-09-13 13:01:06 -0400

    separated the pages

--- constitution/script.js
+++ constitution/script.js
@@ -30,15 +30,4 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     });
-
-    // Smooth scroll for nav ba
-    const navLinks = document.querySelectorAll("nav a");
-    navLinks.forEach((link) => {
-        link.addEventListener("click", function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
-            targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
-        });
-    });
 });
