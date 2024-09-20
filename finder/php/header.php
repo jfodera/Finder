38e3a0525f2dd5593a434a74a5fdf2f8e35a586d
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-09-20 00:17:08 -0400

    FIX PHp

--- finder/php/header.php
+++ finder/php/header.php
@@ -1,12 +1,27 @@
 <header class="global-header">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
     <div class="header-content">
-        <div class="logo">Finder</div>
-        <nav>
-            <ul>
-                <li><a href="about.php">About</a></li>
-                <li><a href="login.php">Login</a></li>
-                <li><a href="register.php">Register</a></li>
-            </ul>
-        </nav>
+        <div class="left-nav">
+            <nav>
+                <ul>
+                    <li><a>Logo Here</a></li>
+                    <li><a href="<?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'index.php' : '../index.php'; ?>">Home</a></li>
+                    <li><a href="<?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'php/about.php' : 'about.php'; ?>">About</a></li>
+                    <li><a href="<?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'php/contact.php' : 'contact.php'; ?>">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="right-nav">
+            <nav>
+                <ul>
+                    <li><a href="<?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'php/login.php' : 'login.php'; ?>">Login</a></li>
+                    <li><a href="<?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'php/register.php' : 'register.php'; ?>">Register</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </header>
