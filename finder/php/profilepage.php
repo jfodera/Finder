0241ee4d85e7f89d1250615f40e04e7b29eea80d
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-09-29 21:36:39 -0400

    Create profilepage.php

---
+++ finder/php/profilepage.php
@@ -0,0 +1,22 @@
+<?php include 'header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Finder - Profile Page</title>
+    <link rel="stylesheet" href="/finder/style.css">
+    
+</head>
+
+<body>
+    <div class="container">
+        
+    </div>
+    <script src="script.js"></script>
+</body>
+
+</html>
