79b3230d47d56fc8da5799276072848b62fe067d
Kagiri2 <37049496+Kagiri2@users.noreply.github.com>
2024-10-07 12:47:05 -0400

    test login

---
+++ db/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'finder_db';
+$username = 'root';
+$password = '1';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
