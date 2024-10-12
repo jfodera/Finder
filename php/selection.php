3a876fe5f84dc38c8921233d84de09500e70339c
zhouh11 <zhouh11@rpi.edu>
2024-10-12 10:19:20 -0400

    add php file

---
+++ php/selection.php
@@ -0,0 +1,29 @@
+<?php include 'header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Finder - Selection</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <div class="container">
+        <section class="about-us">
+            <h1>What Item Are You Recording?</h1>
+            
+        </section>
+        <div class="buttons">
+            <a href="./recorder_info.php" class="button">Find an Item</a>
+            <a href="./recorder_info.php" class="button">Record an Item</a>
+        </div>
+    </div>
+    <script src="../script.js"></script>
+</body>
+
+</html>
