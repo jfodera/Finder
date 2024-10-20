2d809fc9fcdd6ac3a171c5054ab1f00fb5757b3d
zhouh11 <zhouh11@rpi.edu>
2024-10-20 11:44:04 -0400

    edit

--- lost_item_form.js
+++ lost_item_form.js
@@ -1,39 +1,34 @@
-const pages = Array.from(document.querySelector('form .page.page-1'));
-const nextBtn = document.querySelectorAll('form .next-btn');
-const prevBtn = document.querySelectorAll('form .prev-btn');
-const form = document.querySelector('form');
+document.addEventListener("DOMContentLoaded", function() {
+    const pages = Array.from(document.querySelectorAll('#infoForm .page'));
+    const nextBtns = document.querySelectorAll('.next-btn');
+    const prevBtns = document.querySelectorAll('.prev-btn');
 
-console.log(pages);
-// console.log(nextBtn);
-// console.log(prevBtn);
+    nextBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        changePage('next');
+    });
+});
 
-
-
-// nextBtn.forEach(button=>{
-//     button.addEventListener('click', () => {
-//         changePage('next');
-//     })
-// })
-
-// prevBtn.forEach(button=>{
-//     button.addEventListener('click', () => {
-//         changePage('prev');
-//     })
-// })
-
-// function changePage(btn){
-//     let index = 0;
-//     const active = document.querySelector('form .page.active');
-//     index = pages.indexOf(active);
-//     pages[index].classList.remove('active');
-//     if(btn ==='next'){
-//         index ++;
-//     }else if(btn ==='prev'){
-//         index --;
-//     }
-//     pages[index].classList.add('active')
-//     console.log(index)
-// }
+prevBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        changePage('prev');
+    });
+});
 
 
+function changePage(btn){
+    const active = document.querySelector('#infoForm .page.active');
+    let index = pages.indexOf(active);
+    index = pages.indexOf(active);
+    pages[index].classList.remove('active');
+    if(btn ==='next'){
+        index ++;
+    }else if(btn ==='prev'){
+        index --;
+    }
+    pages[index].classList.add('active')
+    console.log(index)
+}
 
+    
+});
--- php/recorder_info.php
+++ php/recorder_info.php
@@ -12,7 +12,7 @@
     <title>Finder - RecorderInfo</title>
     <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="../css/info.css">
-    <script src="../js/lost_item_form.js" defer></script>
+    <!-- <script src="../js/lost_item_form.js" defer></script> -->
 
 
 </head>
@@ -73,6 +73,44 @@
         </div>
 
     </div>
+
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const pages = Array.from(document.querySelectorAll('#infoForm .page'));
+        const nextBtns = document.querySelectorAll('.next-btn');
+        const prevBtns = document.querySelectorAll('.prev-btn');
+
+        nextBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            changePage('next');
+        });
+    });
+
+    prevBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            changePage('prev');
+        });
+    });
+
+    
+    function changePage(btn){
+        const active = document.querySelector('#infoForm .page.active');
+        let index = pages.indexOf(active);
+        index = pages.indexOf(active);
+        pages[index].classList.remove('active');
+        if(btn ==='next'){
+            index ++;
+        }else if(btn ==='prev'){
+            index --;
+        }
+        pages[index].classList.add('active')
+        console.log(index)
+    }
+
+        
+    });
+</script>
 
 
 
