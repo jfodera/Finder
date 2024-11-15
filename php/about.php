f581e0e70e4d18224e73489ba3307f8a939851ee
Cooper Kelly <kellyc9@rpi.edu>
2024-11-15 12:33:55 -0500

    Div

--- php/about.php
+++ php/about.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    <div class="container">
         <div class="content">
             <h2>About Finder</h2>
         </div>
@@ -19,7 +18,6 @@
                 Finder is a lost and found platform dedicated to helping the RPI community recover lost items quickly and easily. Our mission is to create an efficient and user-friendly app that connects lost items with their rightful owners through a smart matching system. By streamlining the process and providing immediate feedback, we aim to reduce the frustration and anxiety of losing personal belongings on campus. Finder offers a secure and reliable solution, empowering users with real-time answers and peace of mind.
             </div>
         </div>
-    </div>
     <script src="../script.js"></script>
 </body>
 </html>
