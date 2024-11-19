d30c71a9db3e258d38d8b59db68874cbf6e0cea5
Kagiri2 <37049496+Kagiri2@users.noreply.github.com>
2024-11-19 13:03:57 -0500

    Update getCourseInfo.php

--- php/getCourseInfo.php
+++ php/getCourseInfo.php
@@ -1,10 +1,14 @@
 <?php
 header('Content-Type: application/json');
 
-$courseInfo = [
-    "course" => "Web Science",
-    "number" => "ITWS 4500",
-    "Description" => "A course about Web Science"
-];
+$requestUri = $_SERVER['REQUEST_URI'];
 
-echo json_encode($courseInfo, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+if ($requestUri === '/itws/4500') {
+    $courseInfo = [
+        "course" => "Web Science",
+        "number" => "ITWS 4500",
+        "Description" => "A course about Web Science"
+    ];
+
+    echo json_encode($courseInfo, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+} else http_response_code(404);
