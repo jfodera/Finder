f8a99d873aad64e3c1200f150f56c28e280cb44b
milesthomas <ths22115@towerhill.org>
2024-12-02 17:15:10 -0500

    removed redundant head element

--- php/header.php
+++ php/header.php
@@ -5,13 +5,6 @@ if (session_status() === PHP_SESSION_NONE) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="../style.css">
-</head>
 <body>
 <header class="global-header">
     <div class="header-content">
