a22dc08776c97b820f42c04c6b8f6972b3863d58
milesthomas <ths22115@towerhill.org>
2024-12-04 18:16:39 -0500

    cleaning out comments

--- php/background-under.php
+++ php/background-under.php
@@ -1,20 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <body>
-        <div class="background under">
-            <!-- <div class="bubble bubble--1"></div>
-            <div class="bubble bubble--2"></div>
-            <div class="bubble bubble--3"></div>
-            <div class="bubble bubble--4"></div>
-            <div class="bubble bubble--5"></div>
-            <div class="bubble bubble--6"></div>
-            <div class="bubble bubble--7"></div>
-            <div class="bubble bubble--8"></div>
-            <div class="bubble bubble--9"></div>
-            <div class="bubble bubble--10"></div>
-            <div class="bubble bubble--11"></div>
-            <div class="bubble bubble--12"></div> -->
-        </div>
+        <div class="background under"></div>
     </body>
     <style>
         .under {
