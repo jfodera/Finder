5d6267753a17ec0296301dcd0cd2603d944b863a
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-12-07 21:34:05 -0500

    php curl

--- php/getApiKey.php
+++
@@ -1,9 +0,0 @@
-<?php
-// getapikey.php
-require_once '../db/db_connect.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-header('Content-Type: application/json');
-echo json_encode(['key' => $_ENV['XAI_API_KEY']]);
