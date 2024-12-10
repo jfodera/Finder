e39f23ec5669148b8261c985d52c3213006e4220
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-12-10 18:42:27 -0500

    revert

--- php/recorder_register.php
+++ php/recorder_register.php
@@ -13,6 +13,7 @@ session_start();
 $headerPath = __DIR__ . '/header.php';
 require_once $headerPath;
 require_once __DIR__ . '/../db/db_connect.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 //verification email functionality
 function sendVerificationEmail($email, $token) {
